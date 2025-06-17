645c82443d67d6e22e64d0d42fcc6f7a1b568817
Paula Mühlbach <pollam@gmail.com>
2025-06-16 21:59:09 -0300

    criando teste para upload de imagem

--- admin/script/script.js
+++ admin/script/script.js
@@ -3,7 +3,7 @@ const CSV_URL = "https://raw.githubusercontent.com/likehomepropriedades/rentabil
 const GITHUB_CSV_URL = "https://raw.githubusercontent.com/likehomepropriedades/rentabilizar/main/data/dados.csv";
 
 // URL do Apps Script responsável apenas pelo upload de imagens
-const API_URL = 'https://script.google.com/macros/s/AKfycbyx8e29uo5bIWBl82s0I_o2Mz9ZPzjW-IDp0Xs1TaqRaL0NWK7SbeFtE9dRO9FZcaz9QA/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbzm-hePmBqjXr63vrpVY5GrE5aEhNDs4jKpsphAwtZutUuAMXtlJiKmaG33nFbEm4NM2g/exec';
 
 const USER_EMAIL = 'paula@likehomepropriedades.com.br';
 
